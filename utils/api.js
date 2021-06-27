5231674cd8807738d25e28d0622c8955356ca77d
NilBrains <167288520@qq.com>
2021-06-27 11:53:19 +0800

    add

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,7 @@
 // export const base_url = "https://nilbrains.utools.club/"
 // export const base_url = "https://jmqs.nilbrains.com/"
-// export const base_url = "http://apitest.nilbrains.com/rider/index"
-export const base_url = "https://api.nilbrains.com/rider/index"
+export const base_url = "http://apitest.nilbrains.com/rider/index"
+// export const base_url = "https://api.nilbrains.com/rider/index"
 
 export const request = (params) => {
   return new Promise((resolve, reject) => {
